4114541271fcaf87fd0b09b408697e30508859ff
AdamKhagar <grinnol289@gmail.com>
2020-08-24 20:01:17 +0300

    fix token import

---
+++ config.py
@@ -0,0 +1,26 @@
+token = '1356654480:AAG8kvXn5x9PVLM8V8UEE4GuMXyuA4CL-48'
+languages = {'English': 'words/English-words.json'}
+send_time_var = {
+    'Утром': 1,
+    'В обед': 2,
+    'Вечером': 3,
+    'Ночью': 4
+}
+
+profiles = {
+    1: ['08:30:00', '11:00:00'],
+    2: ['12:30:00', '15:30:00'],
+    3: ['17:30:00', '19:00:00'],
+    4: ['20:00:00', '23:00:00']
+}
+
+list_path = 'users_data/users_list.json'
+dir_path = 'users_data/user_data#{}'
+f_path = {
+ 'lang': '{}/lang#{}.json',
+ 'progress': '{}/progess#{}.json',
+ 'send_qty': '{}/send_qty#{}.json',
+ 'send_time': '{}/send_time#{}.json',
+ 'state': '{}/state{}.json',
+ 'swap': '{}/swap{}.json'
+}
